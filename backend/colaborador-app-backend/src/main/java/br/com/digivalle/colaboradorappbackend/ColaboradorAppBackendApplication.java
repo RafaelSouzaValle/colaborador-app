9de79a0e539b949abcc4f9a4cd9f2ffc5ab4e254
Rafael S Valle <rafaelsouzavalle@gmail.com>
2024-03-20 02:56:22 -0300

    Update ColaboradorAppBackendApplication.java

--- backend/colaborador-app-backend/src/main/java/br/com/digivalle/colaboradorappbackend/ColaboradorAppBackendApplication.java
+++ backend/colaborador-app-backend/src/main/java/br/com/digivalle/colaboradorappbackend/ColaboradorAppBackendApplication.java
@@ -1,13 +1,20 @@
 package br.com.digivalle.colaboradorappbackend;
 
+import br.com.digivalle.colaboradorappbackend.config.database.DatabaseMigration;
+import br.com.digivalle.colaboradorappbackend.service.PasswordStrengthService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ColaboradorAppBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ColaboradorAppBackendApplication.class, args);
-	}
+		ConfigurableApplicationContext context = SpringApplication.run(ColaboradorAppBackendApplication.class, args);
 
+		DatabaseMigration databaseMigration = context.getBean(DatabaseMigration.class);
+
+		databaseMigration.migrateDatabase();
+	}
 }
